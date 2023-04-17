ddc5322dd8cafca9fa46c7790b080045b5f32d1d
이기림(Kirim Lee)/Biz플랫폼개발팀/SKP <kirim@sk.com>
2023-04-17 23:13:32 +0900

    구조 및하단 이미지

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,8 +5,37 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Home Screen'),
+    return Scaffold(
+      backgroundColor: Colors.pink[100],
+      body: SafeArea(child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _DDay(),
+          _CoupleImage()
+        ],
+      )),
+    );
+  }
+}
+
+// 디데이 계산 위젯
+class _DDay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('DDay Widget');
+  }
+}
+
+// 커플 이미지 위젯
+class _CoupleImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset(
+          "asset/img/middle_img.png",
+          height: MediaQuery.of(context).size.height / 2
+      ),
     );
   }
 }
